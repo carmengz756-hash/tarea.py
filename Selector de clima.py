08e992ad9cbbe3039e02d323de5032c6791bdccc
carmengz756-hash <carmengz756@gmail.com>
2026-04-27 13:00:19 -0500

    Create Selector de clima.py

---
+++ Selector de clima.py
@@ -0,0 +1,11 @@
+#el selector del clima
+clima=input("¿como esta el clima hoy?:")
+match clima:
+    case("soleado"):
+        print("usa ropa lijera")
+    case("lluvioso"):
+        print("cuidado al conducir")
+    case("nevado"):
+        print("lleve ropa abrigadora")
+    case("nublado"):
+        print("esta corriendo viento")
