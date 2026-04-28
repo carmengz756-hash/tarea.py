77f1ae3c33016c2884d0ca0a7c7b97ea76576a65
carmengz756-hash <carmengz756@gmail.com>
2026-04-28 18:53:31 -0500

    Update Selector de clima.py

--- Selector de clima.py
+++ Selector de clima.py
@@ -1,11 +1,12 @@
 #el selector del clima
-clima=input("¿como esta el clima hoy?:")
+clima=input("¿como esta el clima hoy?:").lower()
+print("Soleado","Lluvioso","Nevado","Nublado")
 match clima:
-    case("soleado"):
+    case"soleado":
         print("usa ropa lijera")
-    case("lluvioso"):
+    case"lluvioso":
         print("cuidado al conducir")
-    case("nevado"):
+    case"nevado":
         print("lleve ropa abrigadora")
-    case("nublado"):
+    case"nublado":
         print("esta corriendo viento")
