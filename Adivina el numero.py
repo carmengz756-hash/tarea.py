f4918e344bdd77d306bb202f34b9ebc19865e286
carmengz756-hash <carmengz756@gmail.com>
2026-04-28 16:49:52 -0500

    Create Adivina el numero.py

---
+++ Adivina el numero.py
@@ -0,0 +1,12 @@
+#Adivina el Número: Crea una variable secreto = 7 . Pide al usuario que adivine el número. Dale pistas si es más alto o más bajo, y usa un bucle para que intente hasta acertar
+secreto=7
+
+while True:
+    b=int(input("Adivine el numero:"))
+    if b>secreto:
+        print("es un numero mas bajo")
+    elif b<secreto:
+        print("es un numero mas alto")
+    else:
+        print("correcto!")
+        break
