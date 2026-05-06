6a4baf8246e0062d27c69995ff22403f55093c10
carmengz756-hash <carmengz756@gmail.com>
2026-05-05 20:18:35 -0500

    Update calculadora de pares.py

--- calculadora de pares.py
+++ calculadora de pares.py
@@ -1,9 +1,8 @@
 #Calculadora de Pares: Pide números al usuario. Si el número es par, imprímelo; si  es impar, usa continue para ignorarlo. El bucle termina cuando el usuario escriba 0
-numero=int(input("ingrese numeros"))
-while numero !=0:
-    continue numero:
- print("es un numero par")
+numero=int(input("ingrese numeros:"))
+while numero %2==0:
+    print("es un numero par")
+    break
 else:
-    print("es un numero impar")
-numero=int(input("ingrese otro numero, 0 para salir:"))
+    numero=int(input("ingrese otro numero, 0 para salir:"))
 print("programa finalizado")
