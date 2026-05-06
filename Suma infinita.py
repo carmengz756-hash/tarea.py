88822f74d8aa42a32b4eb64af47450ac48f400cc
carmengz756-hash <carmengz756@gmail.com>
2026-05-05 21:50:15 -0500

    Update Suma infinita.py

--- Suma infinita.py
+++ Suma infinita.py
@@ -1,8 +1,6 @@
 #La Suma Infinita: Pide al usuario que ingrese cuántos números desea sumar. Luego, usa un for para solicitar esos numeros,sumalos y muestra el total
-suma=0
-cantidad=int(input("¿cuantos numeros desea sumar?:"))
-print(cantidad)
-for x in range(cantidad):
-    numero=float(input(f"ingrese el numero {x+1}:"))
-    suma+=numero
-print("la suma total es:",suma)
+numero=int(input("ingrese los numeros que desees sumar: "))
+for x in range(numero):
+   while True:
+    suma= numero + numero
+    print(suma)
