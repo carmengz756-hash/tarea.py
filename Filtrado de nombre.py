6d27afcd95eb4a392c90c62e903e1b4133a25f32
carmengz756-hash <carmengz756@gmail.com>
2026-05-12 20:33:26 -0500

    Update Filtrado de nombre.py

--- Filtrado de nombre.py
+++ Filtrado de nombre.py
@@ -1,5 +1,6 @@
 #Filtrado de nombres: Dada una lista de nombres ["Ana", "Pedro", "Alberto","Maria"],usa un for e if para imprimir solo los nombres que comienzan con la letra ´A´
-nombres={"Ana","Pedro","Alberto","Maria"}
-for nombre in nombres:
-    if nombre.startswith("A"):
-        print(nombre)
+contador_a=0
+lista=["ana","pedro","alberto","maria"]
+for x in lista:
+    if "a" in lista:
+        contador_a+=1 
